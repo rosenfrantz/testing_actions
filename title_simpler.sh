7f93601edec1f981efa755eaa5908866d9d4a09d
rosenfrantz <101673737+rosenfrantz@users.noreply.github.com>
2022-08-31 15:15:31 -0400

    Update title_simpler.sh
    not working yet but want to show Joy

--- title_simpler.sh
+++ title_simpler.sh
@@ -1,4 +1,10 @@
-if grep -m  1 -Rq "title:" "title_contained.md"
-    then echo "true"
-        else echo "false"
-fi
+for FILE in * 
+do
+    if [[ -s "$FILE.md" ]]
+        then 
+            if grep -m  1 -Rq "title:" "title_contained.md"
+                 then echo "true"
+                    else echo "false"
+            fi
+    fi
+done
