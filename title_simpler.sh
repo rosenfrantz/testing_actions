da49cdccdd7cd358d1c9f5dd9cde7938bdab1402
rosenfrantz <101673737+rosenfrantz@users.noreply.github.com>
2022-09-01 10:39:15 -0400

    title contained in all md
    next level abstraction working, wherein script looks at all .md files (except README) to see if there is "title:" contained in the text.

--- title_simpler.sh
+++ title_simpler.sh
@@ -1,8 +1,8 @@
 for FILE in * 
 do
-    if [[ -s "$FILE.md" ]]
+    if [[ $FILE == *.md && $FILE != README.md ]] # restricts only to markdown files OTHER than README (which does not have YAML) ignoring .sh or any other future commited files
         then 
-            if grep -m  1 -Rq "title:" "title_contained.md"
+            if grep -m  1 -Rq "title:" "$FILE"
                  then echo "true"
                     else echo "false"
             fi
